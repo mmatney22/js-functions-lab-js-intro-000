ce9c1dbcb2b5f4f0e9fe2e0cbfffa3badf429b68
Morgan Matney <morgan.matney89@gmail.com>
2020-04-10 14:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyCustomHolidayTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
